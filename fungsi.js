65b92f9c98cb169516273624eae0780100d4784b
Dikha Arianda <dikhaarianda@gmail.com>
2023-02-04 23:35:05 +0700

    fix divide

--- fungsi.js
+++ fungsi.js
@@ -4,10 +4,10 @@ const add = (a,b) => a + b;
 console.log(add(1,2));
 
 const subtract = (a,b) => a - b;
-console.log(subtract(5,2));
+console.log(add(5,2));
 
 const multiplyNumber = (a,b) => a * b;
 console.log(multiplyNumber(2,2));
 
 const divide = (a,b) => a / b;
-console.log(divide(10,2));
+console.log(divide(20,2));
