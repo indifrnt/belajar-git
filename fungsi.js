f64111f0616686c1fbe17c70aa803e192b9d39b0
dikhaarianda <102576990+dikhaarianda@users.noreply.github.com>
2023-02-07 00:01:00 +0700

    Update fungsi.js

--- fungsi.js
+++ fungsi.js
@@ -1,16 +1,19 @@
+//pertambahan
 const add = (a,b) => a + b;
 console.log(add(1,2));
 
+//pengurangan
 const subtract = (a,b) => a - b;
 console.log(add(5,2));
 
+//perkalian
 const multiplyNumber = (a,b) => a * b;
 console.log(multiplyNumber(2,2));
 
+//pembagian
 const divide = (a,b) => a / b;
 console.log(divide(20,2));
 
-console.log(add(5,5));
-
+//sin
 const mathSin = (number) => Math.sin(number);
 console.log(mathSin(10));
